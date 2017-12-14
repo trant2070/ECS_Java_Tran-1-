13894022cc20827fc2fc869427b0459e8d7c3d06
trant2070 <zukari.chucky@gmail.com>
2017-12-14 11:09:39 -0800

    continued work on assignment 8

---
+++ ECSMethods1/src/MethodPass.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MethodPass
+{
+	public static void main ( String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		
+		int n;
+		System.out.print("Pick a number: ");
+		n = keyboard.nextInt();
+			  
+			if ( n % 3 == 0 ) 
+			{ 
+				System.out.println("I like that number!"); 
+			}
+			else  
+			{
+				System.out.println("That number is gross..."); 
+			}
+			keyboard.close();
+		
+	}
+
+}
